ec4f28c3b0d17cfe89f960920ead5e83470a5e16
RohitE25 <kingrohit863@gmail.com>
2024-09-05 09:49:46 +0530

    backend also done

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let body =document.querySelector("body");
 let boxs =document.querySelectorAll(".box");
 let resetBut =document.querySelector("#reset-butt");
+let winDiv =document.querySelector("#winDiv")
+let newGame =document.querySelector(".newGame")
+let main =document.querySelector("#main")
+let h4win = document.querySelector("#wi4")
+
+
 
 let turnX= true;
 
 let winPatterns=[[0,1,2],[0,3,6],[0,4,8],
 [1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+
+
 
 boxs.forEach((box)=>{
     box.addEventListener("click",()=>{
@@ -19,7 +27,48 @@ boxs.forEach((box)=>{
             turnX=true;
         }
         box.disabled=true;
-    })
+        checkPatterns(box);
+    }) 
 })
+const winnerMsg =(winner)=>{
+    h4win.innerHTML =winner +"\tplayer is the"
+    winDiv.classList.remove("hide");
+    main.classList.add("hide");
+}
+const checkPatterns=()=>{
+    for(check of winPatterns){
+        console.log(check[0],check[1],check[2])
+        console.log(boxs[check[0]].innerText,boxs[check[1]].innerText,boxs[check[2]].innerText)
+        let w1 =boxs[check[0]].innerText;
+        let w2 =boxs[check[1]].innerText;
+        let w3 =boxs[check[2]].innerText;
+        if(w1 !=""&& w2 !="" && w3 != ""){
+            if(w1==w2 && w2==w3){
+                console.log("winner",w1)
+                winnerMsg(w1);
+
+            }
+        }
+    }
+}
+// let drawGame =()=>{
+//     for (box of boxs){
+//         box.innerHTML=" ";
+//         box.disabled=false;
+
+//     }
+// }
+let resetGame =()=>{
+    turnX=true;
+    for (box of boxs){
+    box.innerHTML=" ";
+    box.disabled=false;}
+    main.classList.remove("hide");
+    winDiv.classList.add("hide");
+
+}
+resetBut.addEventListener("click",resetGame)
+newGame.addEventListener("click",resetGame)
+
 
 
